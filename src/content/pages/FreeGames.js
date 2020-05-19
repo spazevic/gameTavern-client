3d7cb9a55d95bc38122f66d4c89fbbf26664fc20
garedb <garedbarney@gmail.com>
2020-05-19 16:59:30 -0700

    adding cards and assets

--- src/content/pages/FreeGames.js
+++ src/content/pages/FreeGames.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const FreeGames = props => {
     return (
-        <div>
-            hi
+    	<div>
+        <div class="card">
+            <video src="https://www.addictinggames.com/sites/default/files/forge-of-empires-mmo.mp4" width="200" controls autoplay>
+			</video>
         </div>
+       	</div>
     )
 }
 
