897d5a0689c79d238cf11325c970a3a90ca502c2
garedb <garedbarney@gmail.com>
2020-05-22 10:04:35 -0700

    fixed some minor errors with iframe

--- src/content/pages/FreeGamesPlay.jsx
+++ src/content/pages/FreeGamesPlay.jsx
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 
 
 const FreeGamesPlay = props => {
 	let gameUrl = "https://www.addictinggames.com/embed/html5-games/" + props.freeGame
   return (
     <div class="freegames">
-    <iframe width="800" height="600" src={gameUrl} scrolling="no"></iframe>
+	<iframe width="800" height="600" src={gameUrl} title="freeGamesP" scrolling="no"></iframe>
   	</div>
   )
 }
