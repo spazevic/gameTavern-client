42017c288cd373ecf512a7c1ae84b19ac5744971
ahonore42 <a1honore80@gmail.com>
2020-05-22 16:57:49 -0700

    Added 30 free games

--- src/content/pages/FreeGames.js
+++ src/content/pages/FreeGames.js
@@ -7,129 +7,10 @@ const FreeGames = props => {
     <div>
     	<div class="freeGamesContainer">
             <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/bubblespinner.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(19016)}>
-                    <Link to="/FreeGamesPlay"><b>Bubble Spinner</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/shakes-fidget_01.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(23942)}>
-                    <Link to="/FreeGamesPlay"><b>Shakes & Fidget</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/tanks-io.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(20305)}>
-                    <Link to="/FreeGamesPlay"><b>Tanks</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/geometry-tower.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(22905)}>
-                    <Link to="/FreeGamesPlay"><b>Geometry Tower</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/conflictofnations.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(23088)}>
-                    <Link to="/FreeGamesPlay"><b>Conflict of Nations: WW3</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/the-grand-grimoire-chronicles-episode-2_01.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(23928)}>
-                    <Link to="/FreeGamesPlay"><b>The Grand Grimoir Chronicles</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
                 <video src="https://www.addictinggames.com/sites/default/files/1000-blocks.mp4" width="200" controls autoplay>
                 </video>
                 <div className="freeGameTitle" onClick={() => setFreeGame(23658)}>
                     <Link to="/FreeGamesPlay"><b>1000 Blocks</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/Dropdownjewelblast.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(23948)}>
-                    <Link to="/FreeGamesPlay"><b>Dropdown Jewel Blast</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/AirFighter.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(18484)}>
-                    <Link to="/FreeGamesPlay"><b>Air Fighter</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/theimpossiblequiz.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(18809)}>
-                    <Link to="/FreeGamesPlay"><b>The Impossible Quiz</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/solitaire_1_2.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(23665)}>
-                    <Link to="/FreeGamesPlay"><b>Pirate Cards</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/flappy%20bird%20game%20thumb.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(24032)}>
-                    <Link to="/FreeGamesPlay"><b>Flappy Bird</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/night-race-2020.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(24161)}>
-                    <Link to="/FreeGamesPlay"><b>Night Race</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/jump-bottle.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(23571)}>
-                    <Link to="/FreeGamesPlay"><b>Jump Bottle</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/run-heroes-online.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(24164)}>
-                    <Link to="/FreeGamesPlay"><b>Run Heroes Online</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/zapperio.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(24166)}>
-                    <Link to="/FreeGamesPlay"><b>Zapper</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/knots%20game.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(24027)}>
-                    <Link to="/FreeGamesPlay"><b>Knots</b></Link>
-                </div>
-            </div>
-            <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/solitaire_83.mp4" width="200" controls autoplay>
-                </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(23597)}>
-                    <Link to="/FreeGamesPlay"><b>The Impossible Rise</b></Link>
                 </div>
             </div>
             <div className="gameCard">
@@ -140,10 +21,151 @@ const FreeGames = props => {
                 </div>
             </div>
             <div className="gameCard">
-                <video src="https://www.addictinggames.com/sites/default/files/crazy-stunt-cars-2.mp4" width="200" controls autoplay>
+                <video src="https://www.addictinggames.com/sites/default/files/AirFighter.mp4" width="200" controls autoplay>
                 </video>
-                <div className="freeGameTitle" onClick={() => setFreeGame(23687)}>
-                    <Link to="/FreeGamesPlay"><b>Stunt Park Racer</b></Link>
+                <div className="freeGameTitle" onClick={() => setFreeGame(18484)}>
+                    <Link to="/FreeGamesPlay"><b>Air Fighter</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/battle-coast.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(16716)}>
+                    <Link to="/FreeGamesPlay"><b>Battle Coast</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/bubblespinner.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(19016)}>
+                    <Link to="/FreeGamesPlay"><b>Bubble Spinner</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/catch-it.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(17510)}>
+                    <Link to="/FreeGamesPlay"><b>Catch It</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/conflictofnations.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(23088)}>
+                    <Link to="/FreeGamesPlay"><b>Conflict of Nations: WW3</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/Dropdownjewelblast.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(23948)}>
+                    <Link to="/FreeGamesPlay"><b>Dropdown Jewel Blast</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/flappy%20bird%20game%20thumb.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(24032)}>
+                    <Link to="/FreeGamesPlay"><b>Flappy Bird</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/geometry-tower.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(22905)}>
+                    <Link to="/FreeGamesPlay"><b>Geometry Tower</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/helicoptergame.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(16834)}>
+                    <Link to="/FreeGamesPlay"><b>Helicopter Game</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/jump-bottle.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(23571)}>
+                    <Link to="/FreeGamesPlay"><b>Jump Bottle</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/kittencannon.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(21697)}>
+                    <Link to="/FreeGamesPlay"><b>Kitten Cannon</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/knots%20game.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(24027)}>
+                    <Link to="/FreeGamesPlay"><b>Knots</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/miniputt.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(19829)}>
+                    <Link to="/FreeGamesPlay"><b>Mini Putt</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/nature-strikes-back.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(16717)}>
+                    <Link to="/FreeGamesPlay"><b>Nature Strikes Back</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/night-race-2020.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(24161)}>
+                    <Link to="/FreeGamesPlay"><b>Night Race</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/Pandemic%20Game.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(20449)}>
+                    <Link to="/FreeGamesPlay"><b>Pandemic II</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/solitaire_1_2.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(23665)}>
+                    <Link to="/FreeGamesPlay"><b>Pirate Cards</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/solitaire_2.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(23807)}>
+                    <Link to="/FreeGamesPlay"><b>Relaxr</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/run-heroes-online.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(24164)}>
+                    <Link to="/FreeGamesPlay"><b>Run Heroes Online</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/shakes-fidget_01.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(23942)}>
+                    <Link to="/FreeGamesPlay"><b>Shakes & Fidget</b></Link>
+                </div>
+            </div>
+            
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/spect-.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(16758)}>
+                    <Link to="/FreeGamesPlay"><b>Spect</b></Link>
                 </div>
             </div>
             <div className="gameCard">
@@ -153,6 +175,50 @@ const FreeGames = props => {
                     <Link to="/FreeGamesPlay"><b>Stunt Dirt Bike</b></Link>
                 </div>
             </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/crazy-stunt-cars-2.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(23687)}>
+                    <Link to="/FreeGamesPlay"><b>Stunt Park Racer</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/tanks-io.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(20305)}>
+                    <Link to="/FreeGamesPlay"><b>Tanks</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/the-grand-grimoire-chronicles-episode-2_01.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(23928)}>
+                    <Link to="/FreeGamesPlay"><b>The Grand Grimoir Chronicles</b></Link>
+                </div>
+            </div>
+            
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/theimpossiblequiz.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(18809)}>
+                    <Link to="/FreeGamesPlay"><b>The Impossible Quiz</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/solitaire_83.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(23597)}>
+                    <Link to="/FreeGamesPlay"><b>The Impossible Rise</b></Link>
+                </div>
+            </div>
+            <div className="gameCard">
+                <video src="https://www.addictinggames.com/sites/default/files/zapperio.mp4" width="200" controls autoplay>
+                </video>
+                <div className="freeGameTitle" onClick={() => setFreeGame(24166)}>
+                    <Link to="/FreeGamesPlay"><b>Zapper</b></Link>
+                </div>
+            </div>
+            
         </div>
     </div>
     )
